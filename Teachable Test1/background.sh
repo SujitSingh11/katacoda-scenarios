730870bcb0d3ad209bf273dc68b2abd2e79c09bf
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-23 21:15:58 +0530

    s

--- Teachable Test1/background.sh
+++ Teachable Test1/background.sh
@@ -4,7 +4,7 @@ apt-get update
 ifconfig
 apt install docker.io -y
 systemctl enable docker
-apt install curl
+apt install curl -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt -y update
