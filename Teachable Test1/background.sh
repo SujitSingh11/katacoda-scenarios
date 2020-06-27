dfa8a655d315b66ff02a5d1b08add74c4dc1de14
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-27 11:53:59 +0530

    s

--- Teachable Test1/background.sh
+++ Teachable Test1/background.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-apt install net-tools
-apt-get update
-ifconfig
-apt install docker.io -y
-systemctl enable docker
-apt install curl -y
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt -y update
-apt install -y kubeadm kubectl kubelet kubernetes-cni
+
