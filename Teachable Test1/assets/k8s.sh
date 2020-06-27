55a2a1f887858caa679f83b8f9377a39e99a3556
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-27 11:56:59 +0530

    Update k8s.sh

--- Teachable Test1/assets/k8s.sh
+++ Teachable Test1/assets/k8s.sh
@@ -2,7 +2,7 @@
 
 ip="$(ifconfig | grep -A 1 'ens3' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
-kubeadm init --apiserver-advertise-address=$ip --pod-network-cidr=10.244.0.0/16
+#kubeadm init --apiserver-advertise-address=$ip --pod-network-cidr=10.244.0.0/16
 
 cat> webapp-service.yaml \
 >apiVersion: v1
