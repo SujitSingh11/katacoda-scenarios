f1bf5a5f017002d5508dc6b68d95aa526ba8556b
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-27 12:02:19 +0530

    Update k8s.sh

--- Teachable Test1/assets/k8s.sh
+++ Teachable Test1/assets/k8s.sh
@@ -4,19 +4,19 @@ ip="$(ifconfig | grep -A 1 'ens3' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
 
 #kubeadm init --apiserver-advertise-address=$ip --pod-network-cidr=10.244.0.0/16
 
-cat> webapp-service.yaml\
->apiVersion: v1
->kind: Service
->metadata:
->  name: web-lb
->spec:
->  type: NodePort
->  externalIPs:
->    - $ip
->  ports:
->    - port: 80
->      protocol: TCP
->      targetPort: 80
->  selector:
->    app: webapp
+cat> webapp-service.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: web-lb
+spec:
+  type: NodePort
+  externalIPs:
+    - $ip
+  ports:
+    - port: 80
+      protocol: TCP
+      targetPort: 80
+  selector:
+    app: webapp
 
