bf89a3fa0b5b08cf85e152ed9a1be8eb119a0a80
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-29 14:18:38 +0530

    Update k8s.sh

--- Teachable Test2/assets/k8s.sh
+++ Teachable Test2/assets/k8s.sh
@@ -1,47 +1,27 @@
 #!/bin/bash
 
 ip="$(ifconfig | grep -A 1 'ens3' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-ip2=[[HOST2_IP]]
 
-echo "$ip2"
-#kubeadm init --apiserver-advertise-address=$id --pod-network-cidr=10.244.0.0/16
+kubeadm init --apiserver-advertise-address=$id --pod-network-cidr=10.244.0.0/16
 
-# cat> webapp-service.yaml<<EOF
-# apiVersion: v1
-# kind: Service
-# metadata:
-#   name: web-lb
-# spec:
-#   type: NodePort
-#   externalIPs:
-#     - "[[HOST_IP]]"
-#     - "[[HOST2_IP]]"
-#   ports:
-#     - port: 80
-#       protocol: TCP
-#       targetPort: 80
-#   selector:
-#     app: webapp
-# EOF
+cp /etc/kubernetes/admin.conf $HOME/
+chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
 
-# cp /etc/kubernetes/admin.conf $HOME/
-# chown $(id -u):$(id -g) $HOME/admin.conf
-# export KUBECONFIG=$HOME/admin.conf
+sleep 2
 
-# sleep 2
+kubectl apply -f /opt/weave-kube
+kubectl get pod -n kube-system
+kubeadm token list
 
-# kubectl apply -f /opt/weave-kube
-# kubectl get pod -n kube-system
-# kubeadm token list
+kubectl apply -f api.yaml
+kubectl apply -f api-service.yaml
+kubectl apply -f webapp.yaml
+kubectl apply -f webapp-service.yaml
+kubectl apply -f malicious.yaml
 
-# kubectl apply -f api.yaml
-# kubectl apply -f api-service.yaml
-# kubectl apply -f webapp.yaml
-# kubectl apply -f webapp-service.yaml
-# kubectl apply -f malicious.yaml
-
-# echo "Join the cluster at with this"
-# kubeadm token create --print-join-command
+echo "Join the cluster at with this"
+kubeadm token create --print-join-command
 
 
 
