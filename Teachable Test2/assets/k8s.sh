38b56d3d06b1440bb9cd5a7dd9030e318be2a2a9
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-29 13:01:31 +0530

    s

--- Teachable Test2/assets/k8s.sh
+++ Teachable Test2/assets/k8s.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-cat> webapp-service.yaml<<EOF
-apiVersion: v1
-kind: Service
-metadata:
-  name: web-lb
-spec:
-  type: NodePort
-  externalIPs:
-    - [[HOST_IP]]
-    - [[HOST2_IP]]
-  ports:
-    - port: 80
-      protocol: TCP
-      targetPort: 80
-  selector:
-    app: webapp
-EOF
-
 ip="$(ifconfig | grep -A 1 'ens3' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
 kubeadm init --apiserver-advertise-address=$id --pod-network-cidr=10.244.0.0/16
@@ -29,12 +11,6 @@ kubectl apply -f /opt/weave-kube
 kubectl get pod -n kube-system
 kubeadm token list
 
-join = kubeadm token create --print-join-command
-
-echo "$join"
-
-echo "$join" >> host2.sh
-
 sleep 5
 
 kubectl apply -f api.yaml
@@ -42,6 +18,9 @@ kubectl apply -f api-service.yaml
 kubectl apply -f webapp.yaml
 kubectl apply -f webapp-service.yaml
 kubectl apply -f malicious.yaml
+
+echo "Join the cluster at with this"
+kubeadm token create --print-join-command
 
 
 
--- Teachable Test2/background.sh
+++ Teachable Test2/background.sh
@@ -1,1 +1,20 @@
-#
+#!/bin/bash
+
+cat> webapp-service.yaml<<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: web-lb
+spec:
+  type: NodePort
+  externalIPs:
+    - [[HOST_IP]]
+    - [[HOST2_IP]]
+  ports:
+    - port: 80
+      protocol: TCP
+      targetPort: 80
+  selector:
+    app: webapp
+EOF
+
