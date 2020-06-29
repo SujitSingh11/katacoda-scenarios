8641c4a3710e4010fea7fae9be265c8f539a59c5
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-29 12:21:58 +0530

    s

--- Teachable Test2/background.sh
+++ Teachable Test2/background.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+kubeadm init --apiserver-advertise-address=[[HOST_IP]] --pod-network-cidr=10.244.0.0/16
+
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+kubectl apply -f /opt/weave-kube
+
+kubectl apply -f api.yaml
+kubectl apply -f api-service.yaml
+kubectl apply -f webapp.yaml
+kubectl apply -f webapp-service.yaml
+kubectl apply -f malicious.yaml
+
 cat> webapp-service.yaml<<EOF
 apiVersion: v1
 kind: Service
