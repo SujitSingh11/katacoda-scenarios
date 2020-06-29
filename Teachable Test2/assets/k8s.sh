149ff41983d4efafc5b4ca48d0771dbc4edcab1a
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-29 12:54:58 +0530

    s

--- Teachable Test2/assets/k8s.sh
+++ Teachable Test2/assets/k8s.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-ip="$(ifconfig | grep -A 1 'ens3' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-
-kubeadm init --apiserver-advertise-address=$id --pod-network-cidr=10.244.0.0/16
-
-sudo cp /etc/kubernetes/admin.conf $HOME/
-sudo chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
-kubectl apply -f /opt/weave-kube
-
-join = kubeadm token create --print-join-command
-
-echo $join
-
-echo $join >> host2.sh
-
-sleep 5
-
 cat> webapp-service.yaml<<EOF
 apiVersion: v1
 kind: Service
@@ -34,6 +17,25 @@ spec:
   selector:
     app: webapp
 EOF
+
+ip="$(ifconfig | grep -A 1 'ens3' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+
+kubeadm init --apiserver-advertise-address=$id --pod-network-cidr=10.244.0.0/16
+
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+kubectl apply -f /opt/weave-kube
+kubectl get pod -n kube-system
+kubeadm token list
+
+join = kubeadm token create --print-join-command
+
+echo "$join"
+
+echo "$join" >> host2.sh
+
+sleep 5
 
 kubectl apply -f api.yaml
 kubectl apply -f api-service.yaml
--- Teachable Test2/step2.md
+++ Teachable Test2/step2.md
@@ -1,3 +1,2 @@
 # Join the network on the host
 
-`bash host2.sh`{{execute HOST2}}
