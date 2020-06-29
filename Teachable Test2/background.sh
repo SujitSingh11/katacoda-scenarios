238eefba9b228e1ff70c37ec3858b476a030414c
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-29 13:34:19 +0530

    Update background.sh

--- Teachable Test2/background.sh
+++ Teachable Test2/background.sh
@@ -2,3 +2,21 @@
 
 
 
+cat> webapp-service.yaml<<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: web-lb
+spec:
+  type: NodePort
+  externalIPs:
+    - [[HOST_IP]]
+    - [[HOST2_IP]]
+  ports:
+    - port: 80
+      protocol: TCP
+      targetPort: 80
+  selector:
+    app: webapp
+EOF
+```{{execute HOST1}}
