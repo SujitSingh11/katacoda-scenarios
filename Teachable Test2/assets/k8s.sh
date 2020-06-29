0c4c0a62e8e3dc5b012e3838e1fa21780a394f44
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-29 13:31:33 +0530

    Update k8s.sh

--- Teachable Test2/assets/k8s.sh
+++ Teachable Test2/assets/k8s.sh
@@ -6,6 +6,24 @@ kubeadm init --apiserver-advertise-address=$id --pod-network-cidr=10.244.0.0/16
 
 sleep 5
 
+cat> webapp-service.yaml<<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: web-lb
+spec:
+  type: NodePort
+  externalIPs:
+    - [[HOST_IP]]
+    - [[HOST2_IP]]
+  ports:
+    - port: 80
+      protocol: TCP
+      targetPort: 80
+  selector:
+    app: webapp
+EOF
+
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
@@ -13,6 +31,7 @@ kubectl apply -f /opt/weave-kube
 kubectl get pod -n kube-system
 kubeadm token list
 
+sleep 2
 
 kubectl apply -f api.yaml
 kubectl apply -f api-service.yaml
