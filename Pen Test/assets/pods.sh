25fef0108bc24a5201ef4b452a10e34be3fa5021
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-07-01 15:04:48 +0530

    s

--- Pen Test/assets/pods.sh
+++ Pen Test/assets/pods.sh
@@ -10,4 +10,3 @@ kubectl apply -f api.yaml
 kubectl apply -f api-service.yaml
 kubectl apply -f webapp.yaml
 kubectl apply -f webapp-service.yaml
-# kubectl apply -f malicious.yaml
--- Pen Test/assets/service.sh
+++ Pen Test/assets/service.sh
@@ -1,6 +1,12 @@
 
+
 kubectl create namespace rbac-ns
 kubectl apply -f service.yaml
 kubectl apply -f role.yaml
 kubectl apply -f rbac-rolebinding.yaml
 kubectl apply -f api-ns.yaml
+
+kubectl apply -f service2.yaml
+kubectl apply -f clusterrole.yaml
+kubectl apply -f clusterrole-binding.yaml
+kubectl apply -f apiv2.yaml
